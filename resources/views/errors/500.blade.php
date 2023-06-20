83162d17ba068d19a20f1392e58987a7e86adb3e
AhmadHRai <ahmadhalraie@gmail.com>
2023-06-20 23:45:42 +0300

    Init for old branch

--- resources/views/errors/500.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-@extends('errors::layout')
-
-@section('title','errors')
-
-
-@section('message','Data is being updated ..... ')
-    
--- resources/views/includes/mapxml.blade.php
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<markers>
-  @foreach($maps as $map)
-  <marker id="{{$map->id}}" name="{{$map->name}}" address="{{$map->address}}" 
-    lat="{{$map->lat}}" lng="{{$map->lng}}" type="{{$map->type}}" />
-  @endforeach
-</markers> 
